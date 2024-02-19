7adba20c6f32f72f1fd8ada70d576fd09fa3ea3e
MichaelDev11 <michael.p011404@gmail.com>
2024-02-18 21:46:13 -0500

    update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar.tsx'
 
-import Home from './pages/home.tsx'
-import AboutMe from './pages/about-me.tsx'
-import ContactMe from './pages/contact-me.tsx'
-import Projects from './pages/projects.tsx'
+import Home from './redirects/home.tsx'
+import AboutMe from './redirects/about-me.tsx'
+import ContactMe from './redirects/contact-me.tsx'
+import Projects from './redirects/projects.tsx'
 
 function App() {
   return (
